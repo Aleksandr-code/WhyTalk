1b61bb8bddaaa9f90bf0944b77e835eebcf437fa
Aleksandr-code <Aleksandr.chess@mail.ru>
2021-03-23 18:08:24 +0600

    edit room-tabs-js

--- app/js/main.js
+++ app/js/main.js
@@ -1,63 +1,23 @@
-var $tabs = function (target) {
-    var
-      _elemTabs = (typeof target === 'string' ? document.querySelector(target) : target),
-      _eventTabsShow,
-      _showTab = function (tabsLinkTarget) {
-        var tabsPaneTarget, tabsLinkActive, tabsPaneShow;
-        // console.log(tabsLinkTarget)
-        tabsPaneTarget = document.querySelector(tabsLinkTarget.getAttribute('href'));
-        tabsLinkActive = tabsLinkTarget.parentElement.querySelector('.communicate-items-active');
-        tabsPaneShow = tabsPaneTarget.parentElement.querySelector('.communicate-items-pane-show');
-        // если следующая вкладка равна активной, то завершаем работу
-        if (tabsLinkTarget === tabsLinkActive) {
-          return;
-        }
-        // удаляем классы у текущих активных элементов
-        if (tabsLinkActive !== null) {
-          tabsLinkActive.classList.remove('communicate-items-active');
-        }
-        if (tabsPaneShow !== null) {
-          tabsPaneShow.classList.remove('communicate-items-pane-show');
-        }
-        // добавляем классы к элементам (в завимости от выбранной вкладки)
-        tabsLinkTarget.classList.add('communicate-items-active');
-        tabsPaneTarget.classList.add('communicate-items-pane-show');
-        document.dispatchEvent(_eventTabsShow);
-      },
-      _switchTabTo = function (tabsLinkIndex) {
-        var tabsLinks = _elemTabs.querySelectorAll('.communicate-items-link');
-        if (tabsLinks.length > 0) {
-          if (tabsLinkIndex > tabsLinks.length) {
-            tabsLinkIndex = tabsLinks.length;
-          } else if (tabsLinkIndex < 1) {
-            tabsLinkIndex = 1;
-          }
-          _showTab(tabsLinks[tabsLinkIndex - 1]);
-        }
-      };
+// Настройка табов
+let jsLinks = document.querySelectorAll('.js-communicate-items-link')
+let jsPanes = document.querySelectorAll('.js-communicate-items-pane')
 
-    _eventTabsShow = new CustomEvent('tab.show', { detail: _elemTabs });
-
-    _elemTabs.addEventListener('click', function (e) {
-      var tabsLinkTarget = e.target;
-      // завершаем выполнение функции, если кликнули не по ссылке
-      if (!tabsLinkTarget.classList.contains('communicate-items-link')) {
-        return;
-      }
-      // отменяем стандартное действие
-      e.preventDefault();
-      _showTab(tabsLinkTarget);
-    });
-
-    return {
-      showTab: function (target) {
-        _showTab(target);
-      },
-      switchTabTo: function (index) {
-        _switchTabTo(index);
-      }
+jsLinks.forEach(function(link){
+  link.addEventListener('click', function(){
+    if(this.classList.contains('active')){
+      return
     }
+    let id = this.getAttribute('data-tab'),
+        pane = document.querySelector('.js-communicate-items-pane[data-tab="'+id+'"]')
+        activeLink = document.querySelector('.js-communicate-items-link.active')
+        activePane = document.querySelector('.js-communicate-items-pane.active')
+    
+    activeLink.classList.remove('active')
+    link.classList.add('active')
 
-  };
+    activePane.classList.remove('active');
+    pane.classList.add('active');
 
-  $tabs('.communicate-items');
+  })
+})
+
