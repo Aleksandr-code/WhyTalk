ed0eaf581b65aa163b5c0fa3470a003654c99676
Aleksandr-code <Aleksandr.chess@mail.ru>
2021-04-06 00:27:40 +0600

    Addaptive added

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,30 @@
+// Меню (мобильная версия)
+document.querySelector('.header__burger-menu').addEventListener('click', function() {
+  let header__box = document.querySelector('.header__box');
+  if (!header__box.classList.contains('header__box-active')){
+    header__box.classList.add('header__box-active')
+    //Анимация
+    let start = Date.now();
+    let timer = setInterval(function() {
+      let timePassed = Date.now() - start;
+      document.querySelector('.header__box-active').style.top = timePassed / 5 + 'px';
+      if (timePassed >= 440) clearInterval(timer);
+    }, 20);
+    // смена значка
+    this.lastElementChild.remove();
+    this.firstElementChild.style = 'transform: rotate(45deg)';
+    this.lastElementChild.style = 'margin-top: -2px; transform: rotate(-45deg)';
+  } else {
+    header__box.classList.remove('header__box-active');
+    // смена значка
+    this.firstElementChild.style = 'transform: rotate(0)';
+    this.lastElementChild.style = 'margin-top: 5px; transform: rotate(0deg)';
+    this.append(document.createElement('div'))
+  }
+
+})
+
+
 // Настройка табов
 let jsLinks = document.querySelectorAll('.js-communicate-items-link')
 let jsPanes = document.querySelectorAll('.js-communicate-items-pane')
@@ -20,4 +47,6 @@ jsLinks.forEach(function(link){
 
   })
 })
+
+
 
