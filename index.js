40de95c8a25472c05ada93236ef59b47967c36e2
Aleksandr-code <Aleksandr.chess@mail.ru>
2021-04-06 20:55:14 +0600

    Added views in project

--- index.js
+++ index.js
@@ -1,28 +1,51 @@
 const express = require('express')
 const path = require('path')
+const exphbs = require('express-handlebars')
+const session = require('express-session')
+const MongoStore = require('connect-mongodb-session')(session)
 const app = express()
 const mongoose = require('mongoose')
+
+const MONGODB_URI = `mongodb+srv://Aleksandr:0v9tgCVWtNRkFKdT@cluster0.4qt7w.mongodb.net/WhyTalk`
+
+const hbs = exphbs.create({
+    defaultLayout : 'main',
+    extname : 'hbs',
+    runtimeOptions: {
+        allowProtoPropertiesByDefault: true,
+        allowProtoMethodsByDefault: true
+    }
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
 
 app.use(express.static(path.join(__dirname,'/app')))
 
 app.get('/', (req, res) => {
-    res.send('<h2>Test new</h2>')
+    res.render('index')
 })
 
 const PORT = process.env.PORT || 3000
 
-/*try{
-    mongoose.connect('mongodb+srv://Aleksandr:0v9tgCVWtNRkFKdT@cluster0.4qt7w.mongodb.net/WhyTalk', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false
-    })
-    console.log('Connect DB')
+async function start(){
+    try{
+        // await mongoose.connect(MONGODB_URI, {
+        //     useNewUrlParser: true,
+        //     useUnifiedTopology: true,
+        //     useFindAndModify: false
+        // })
+        // console.log('Connect DB')
+        app.listen(PORT, ()=> {
+            console.log(`Server is running on ${PORT} port`)
+        })
+    }
+    catch(e){
+        console.log(e)
+    }
 }
-catch(e){
-    console.log(e)
-}*/
 
-app.listen(PORT, ()=> {
-    console.log(`Server is running on ${PORT} port`)
-})
+start()
+
+
